852bb4cfad67be331bd5e23c19ffe430962b2ade
Hoang Ngo <nzo8oanz34@gmail.com>
2024-09-12 04:47:00 +0000

    #12632: Refactor moreh_layer_norm

--- ttnn/cpp/ttnn/operations/moreh/moreh_pybind.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_pybind.cpp
@@ -15,6 +15,8 @@
 #include "ttnn/operations/moreh/moreh_nll_loss_backward/moreh_nll_loss_backward_pybind.hpp"
 #include "ttnn/operations/moreh/moreh_nll_loss_unreduced_backward/moreh_nll_loss_unreduced_backward_pybind.hpp"
 #include "ttnn/operations/moreh/moreh_sum/moreh_sum_pybind.hpp"
+#include "ttnn/operations/moreh/moreh_layer_norm/moreh_layer_norm_pybind.hpp"
+#include "ttnn/operations/moreh/moreh_layer_norm_backward/moreh_layer_norm_backward_pybind.hpp"
 
 namespace ttnn::operations::moreh {
 void bind_moreh_operations(py::module &module) {
@@ -29,5 +31,7 @@ void bind_moreh_operations(py::module &module) {
     moreh_nll_loss_unreduced_backward::bind_moreh_nll_loss_unreduced_backward_operation(module);
     moreh_nll_loss_backward::bind_moreh_nll_loss_backward_operation(module);
     moreh_matmul::bind_moreh_matmul_operation(module);
+    moreh_layer_norm::bind_moreh_layer_norm_operation(module);
+    moreh_layer_norm_backward::bind_moreh_layer_norm_backward_operation(module);
 }
 }  // namespace ttnn::operations::moreh
